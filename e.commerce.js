c4209d0ee9392aa656700a3591cb4b5e46515bb5
anshuchoudhary335 <81526752+anshuchoudhary335@users.noreply.github.com>
2021-04-13 15:15:41 +0530

    Add files via upload

---
+++ e.commerce.js
@@ -0,0 +1,51 @@
+console.log("hello mem");
+       //1. alert("me");
+       //2.document.write("that is men part is project")
+       //3. javaScript console API
+       /*
+       console.warn("warning");
+       console.error("this is a error");
+       console.clear("clear");
+       console.assert(4==6)
+       */
+       //4. javaScript Variables
+       // what are Variables?- containars to store to store data values 
+       var number1=34;
+       var number2=56;
+       console.log(number1+number2);
+       //5. data type in javaScript
+       //strings
+       var str1="this is a string";
+       var str2="this is a string";
+       //Objects
+       //var marks={
+           ram:55,
+           shubham:78,
+        anshuman:99.977
+       }
+       //booleans
+       console.log(marks);
+       var a= true;
+       var b= false;
+       console.log(a,b);
+       var und;
+       console.log(und);
+       var n=null;
+       console.log(n);
+       /*
+       at a very high level , there are two types of types in javaScript
+       1. primitive data types : undefined, null, number,string , boolean, symbol
+       2. reference data types: arrys and objects 
+       */
+       var arr=[1,2,3,4,5,]
+       console.log(arr)
+        Operators in javaScript
+       vor a=34;
+       vor b=56;
+       console.log("the value of a + b is ",a+b);
+       console.log("the value of a - b is ",a-b);
+       console.log("the value of a * b is ",a*b);
+       console.log("the value of a / b is ",a/b);
+       // assignment Operators 
+       vor c=b;
+       console.log(c);
